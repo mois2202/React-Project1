32b53baa2a3db86b6eda7cb2488a9911a6b824ab
Moises Ochoa <156969614+mois2202@users.noreply.github.com>
2024-04-13 22:08:28 -0400

    Day 4
    Gran avance en cuanto a la logica, y funcionamiento, detalles por mejorar en cuanto a la implementacion de clases y cosas por aclarar en cuanto a funcioamiento interno, aunque se podria decir que ya funciona

--- projects/01-React/src/App.jsx
+++ projects/01-React/src/App.jsx
@@ -1,3 +1,8 @@
+/**
+ * Checks if there is a winner on the board.
+ * @param {Array} board - The current state of the board.
+ * @returns {string|null} - The symbol of the winner ('X' or 'O') or null if there is no winner yet.
+ */
 import { useImperativeHandle, useState } from 'react'
 import './App.css'
 
@@ -7,7 +12,7 @@ const TURNS = {
 }
 
 const Square = ({children, isSelected, updateBoard, index}) => {
-
+  
   const handleClick = () => {
     updateBoard(index)
   }
@@ -18,22 +23,69 @@ const Square = ({children, isSelected, updateBoard, index}) => {
   )
 }
 
+const winnerCombinations = [
+  [0, 1, 2], [3, 4, 5], [6, 7, 8], // Horizontal
+  [0, 3, 6], [1, 4, 7], [2, 5, 8], // Vertical
+  [0, 4, 8], [2, 4, 6] // Diagonal
+]
+
+
 function App() {
 
   const [board, setBoard] = useState(Array(9).fill(null))
 
+  const [winner, setWinner] = useState(null) //null is haven't winner yet
+
   const [turn, setTurn] = useState(TURNS.X)
 
+  
+
+  const checkWinner = (board) => { 
+    for (const combination of winnerCombinations) {
+      const [a, b, c] = combination;
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return board[a];
+      }
+    }
+    return null;
+  }
+
+
   const updateBoard = (index) => {
+    if (board[index] !== null || winner !== null) 
+    return
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
+
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    const checkEndGame = (board) => {
+      return newBoard.every((cell) => cell !== null)
+    }
+
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false)
+    }
   }
+
+const resetGame = () => { 
+  setBoard(Array(9).fill(null))
+  setWinner(null)
+  setTurn(TURNS.X)
+}
+
+
+
+
   return (
     <main className='board'>
       <h1> La vieja </h1>
+      <buttom onClick={resetGame}>Reiniciar</buttom>
       <section className='game'>
         {
         board.map((_, index) => {
@@ -50,6 +102,10 @@ function App() {
         }
       </section>
 
+
+
+
+
       <section className='turn'>
         <Square isSelected={turn === TURNS.X}>
           {TURNS.X}
@@ -60,9 +116,33 @@ function App() {
         </Square>
 
       </section>
+
+      <section>
+       {winner != null && (
+
+      <section className='winner'>
+          <div className='text'> 
+            <h2>
+              {winner === false ? 'Empate':'El Ganador es:'} 
+            </h2> 
+
+            <header className='win'> 
+              {winner && <Square>{winner}</Square>}
+            </header>
+
+            <footer>
+              <buttom onClick={resetGame}>Empezar de nuevo</buttom>
+            </footer>
+
+          </div>
+      </section>
+)} 
+
+
+      </section>
+
     </main>
   )
 }
 
-export default App
-
+export default App;
